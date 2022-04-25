d5e2113bd8d62a9a83a8bf4b1a7915f7d18e4ac6
nurulamin092 <mnurulamin092@gmial.com>
2022-04-25 16:29:27 +0600

    find one service

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -12,19 +12,37 @@ app.use(express.json());
 
 // database connection
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qo3jo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://dbuser1:QYh9J5QmVJzcyHhz@cluster0.qo3jo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('genius car db connected');
-    // perform actions on the collection object
-    client.close();
-});
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db("geniusCar").collection('service');
 
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services);
+        });
 
-app.get('/user', (req, res) => {
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: ObjectId(id) };
+            const cursor = serviceCollection.findOne(query)
+            res.send(cursor);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir());
+
+/* app.get('/user', (req, res) => {
     res.send('Running Genius Service ')
-});
+}); */
 
 app.listen(port, () => {
     console.log('listing to prot', port);
