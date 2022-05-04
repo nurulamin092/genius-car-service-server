c7e1ebe85a904cb3a8f94fc90b9ee79e509679a3
nurulamin092 <mnurulamin092@gmial.com>
2022-05-04 11:53:09 +0600

    order collection api create

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ app.use(express.json());
 
 // database connection
 
-const uri = "mongodb+srv://dbuser1:QYh9J5QmVJzcyHhz@cluster0.qo3jo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qo3jo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("geniusCar").collection('service');
-
+        const orderCollection = client.db("geniusCar").collection('order');
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query)
@@ -47,7 +47,15 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const result = await serviceCollection.deleteOne(query);
             res.send(result);
-        })
+        });
+
+        //order collection api
+
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
     }
     finally {
 
@@ -55,9 +63,9 @@ async function run() {
 }
 run().catch(console.dir());
 
-/* app.get('/user', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Running Genius Service ')
-}); */
+});
 
 app.listen(port, () => {
     console.log('listing to prot', port);
